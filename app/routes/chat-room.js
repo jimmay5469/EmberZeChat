8c04ffcb203d7ec8ea1c65c216b9e85a8b18e616
jimmay5469 <jimmay5469@gmail.com>
2015-04-23 15:11:06 -0400

    Add ember-websockets

--- app/routes/chat-room.js
+++ app/routes/chat-room.js
@@ -10,6 +10,19 @@ export default Ember.Route.extend({
   model: function() {
     return this.get('store').find('message');
   },
+  socketService: Ember.inject.service('websockets'),
+  init: function() {
+    this._super.apply(this, arguments);
+    var socket = this.get('socketService').socketFor('wss://ahalogy.fwd.wf/');
+    socket.on('open', this.myOpenHandler, this);
+    socket.on('message', this.myMessageHandler, this);
+  },
+  myOpenHandler: function(event) {
+    console.log('On open event has been called: ' + event);
+  },
+  myMessageHandler: function(event) {
+    console.log('Message: ' + event.data);
+  },
   actions: {
     postMessage: function() {
       this.get('store').createRecord('message', {
@@ -17,6 +30,8 @@ export default Ember.Route.extend({
         user: this.get('session.user')
       }).save();
       this.set('controller.newMessage', '');
+      var socket = this.get('socketService').socketFor('wss://ahalogy.fwd.wf/');
+      socket.send('Hello Websocket World', true);
     }
   }
 });
